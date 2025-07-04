658630270db2b260d6430dc1f168740a6282a841
Nishat <nish.jahan859@gmail.com>
2025-07-04 15:15:41 +0530

    Removed unnecessary functions.

--- app/src/main/java/com/nishat/myalarm/MainActivity.kt
+++ app/src/main/java/com/nishat/myalarm/MainActivity.kt
@@ -25,18 +25,4 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyAlarmTheme {
-        Greeting("Android")
-    }
-}
